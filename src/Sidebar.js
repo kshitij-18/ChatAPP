3387453573c7edb73cb0d2b882dab273ea3a7822
Kshitij <kshitijnath@gmail.com>
2020-08-28 21:00:52 +0530

    setup-fb_db and made 50% sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Sidebar.css'
+import { Avatar, IconButton } from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLargeOutlined'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { SearchOutlined } from '@material-ui/icons'
+import SideBarChat from './SideBarChat'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__header'>
+                <Avatar />
+                <div className='sidebar__headerIconsR'>
+                    <IconButton>
+                        <DonutLargeIcon className='sidebar__icons'></DonutLargeIcon>
+
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon className='sidebar__icons'></ChatIcon>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon className='sidebar__icons'></MoreVertIcon>
+                    </IconButton>
+
+
+                </div>
+            </div>
+
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlined></SearchOutlined>
+                    <input placeholder='Search or start a new Chat' type='text'></input>
+                </div>
+
+            </div>
+
+            <div className='sidebar__chats'>
+                <SideBarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWlEgwmJ6E5jVLOcjY3X0aP3DRKu4i0sk",
+    authDomain: "chat-app-5bf59.firebaseapp.com",
+    databaseURL: "https://chat-app-5bf59.firebaseio.com",
+    projectId: "chat-app-5bf59",
+    storageBucket: "chat-app-5bf59.appspot.com",
+    messagingSenderId: "741027675097",
+    appId: "1:741027675097:web:4709e3b99527cd5d1477c8",
+    measurementId: "G-YE4HDF9VBE"
+};
